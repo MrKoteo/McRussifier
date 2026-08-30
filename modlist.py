905b3b96073533f1429a465df0923d9ed4d3a8d6
MrKoteo <MrKoteo@users.noreply.github.com>
2026-08-30 23:27:01 +0700

    Upd to v2.3
    v2.3
    Added:
    animus, apotheosis, caves_n_cliffs, compacter, cookingforblockheads, danknull, densemetals, dimensionaledibles, dj2addons, effortlessbuilding, fenceoverhaul, framedcompactdrawers, illagers_plus, integrationforegoing, mcwbridges, mcwlights, mekanism, mekores, modularmachineryaddons, moreplates, moretcon, mysticalworld, netherblocks, netherite, openblocks, oreberries, prodigytech, rsgauges, shadowmc, stackie, stygian, taiga, tinkersaether, torcherino, trumpetskeleton, xreliquary, yoyos
    
    Edited:
    industrialforegoing, mod_lavacow, morefurnaces, projectred, ic2

--- modlist.py
+++ modlist.py
@@ -1,6 +1,12 @@
-import os
+import os, subprocess
+
+def copy(txt: str):
+    subprocess.run(["clip"],input=str(txt),text=True,check=True,shell=True,)
 
 dirs = [entry.name for entry in os.scandir('assets') if entry.is_dir()]
 print(len(dirs))
-print(' | '.join(sorted(dirs)))
+res = ' | '.join(sorted(dirs))
+print(res)
+copy(res)
+print('\nСкопировано в буфер обмена!')
 
